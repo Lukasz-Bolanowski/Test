941cba139cad4790177d496ef351d4094586becc
Lukasz-Bolanowski <61976130+Lukasz-Bolanowski@users.noreply.github.com>
2022-06-26 10:55:33 +0200

    Add files via upload

--- hamburgerMenu.js
+++ hamburgerMenu.js
@@ -1,4 +1,6 @@
 const menu = document.querySelector('.hamburger')
+const liItems = document.querySelectorAll('.menu__item a')
+const aside = document.querySelector('aside')
 
 class HandleMenu {
     constructor(menu) {
@@ -9,16 +11,21 @@ class HandleMenu {
         this.menu.classList.toggle('active')
     }
 
-    delay() {
-        this.menu.classList.toggle('active')
+    hideMenu() {
+        liItems.forEach(li => {
+            menu.classList.remove('active')
+        })
     }
 }
 
 const handleMenu = new HandleMenu(menu)
 const toggler = () => handleMenu.toggleMenu()
-const delay = () => handleMenu.delay()
+const hide = () => handleMenu.hideMenu()
 
 menu.addEventListener('click', (toggler))
-menu.addEventListener('blur', () => {
-    setTimeout((delay), 300)
-})
+aside.addEventListener('click', (event) => {
+    const id = event.target.dataset.id
+    if (id) {
+        setTimeout(() => hide(), 250)
+    }
+ })
